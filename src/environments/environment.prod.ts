063c280febd6392f1861085911a1c5845096be7f
Pavan Vishwakarma <pawanmvishwakarma@gmail.com>
2019-04-16 12:00:00 +0530

    my first git project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDPYvtmPye0BerIyM4JldUlv4oi_Zav8JU",
+    authDomain: "oshop-80e4b.firebaseapp.com",
+    databaseURL: "https://oshop-80e4b.firebaseio.com",
+    projectId: "oshop-80e4b",
+    storageBucket: "",
+    messagingSenderId: "123300659098"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDPYvtmPye0BerIyM4JldUlv4oi_Zav8JU",
+    authDomain: "oshop-80e4b.firebaseapp.com",
+    databaseURL: "https://oshop-80e4b.firebaseio.com",
+    projectId: "oshop-80e4b",
+    storageBucket: "",
+    messagingSenderId: "123300659098"
+  }
 };
 
 /*
